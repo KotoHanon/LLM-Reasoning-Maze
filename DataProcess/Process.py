9de14a47e4a5e52e7e38cd5cdd71bfb69bbf0378
KotoHanon <148569017+KotoHanon@users.noreply.github.com>
2025-03-02 22:23:50 +0800

    Update Process.py

--- DataProcess/Process.py
+++ DataProcess/Process.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
 data = pd.read_csv("../env/train_data.csv")[1:]
-def str_process(str):
-    return str.replace("[","").replace("]","").replace(".","")
-data["map"] = data["map"].apply(str_process)
+def map_process(s):
+    s = s.replace("[","").replace("]","").replace(".","").replace(" ","").replace("-1","3").replace('"','')
+    s += "\n"
+    return s
+def action_process(s):
+    return s.replace("'down'","D").replace("'up'","U").replace("'left'","L").replace("'right'","R").replace(",","").replace("[","").replace("]","").replace(" ","")
 
-data.to_csv("../env/train_data.csv",index=False)
+data["map"] = data["map"].apply(map_process)
+data["action_seq"] = data["action_seq"].apply(action_process)
+
+data[:-10].to_csv("../env/train_data.csv",index=False)
+data[-10:].to_csv("../env/test_data.csv",index=False)
