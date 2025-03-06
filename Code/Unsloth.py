5107ccf0bc80266907a251c6c5973e14f4eba57e
KotoHanon <148569017+KotoHanon@users.noreply.github.com>
2025-03-07 03:17:09 +0800

    Update Unsloth.py

--- Code/Unsloth.py
+++ Code/Unsloth.py
@@ -9,23 +9,25 @@ import torch
 max_seq_length = 512 # Can increase for longer reasoning traces
 lora_rank = 32 # Larger rank = smarter, but slower
 
-model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "meta-llama/meta-Llama-3.1-8B-Instruct",
-    max_seq_length = max_seq_length,
-    load_in_4bit = True, # False for LoRA 16bit
-    fast_inference = True, # Enable vLLM fast inference
-    max_lora_rank = lora_rank,
-    gpu_memory_utilization = 0.6, # Reduce if out of memory
-)
+def get_model_and_tokenizer(model_name="QWen/QWen2.5-14B-Instruct"):
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name = model_name,
+        max_seq_length = max_seq_length,
+        load_in_4bit = True, # False for LoRA 16bit
+        fast_inference = True, # Enable vLLM fast inference
+        max_lora_rank = lora_rank,
+        gpu_memory_utilization = 0.6, # Reduce if out of memory
+    )
 
-model = FastLanguageModel.get_peft_model(
-    model,
-    r = lora_rank, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
-    target_modules = [
-        "q_proj", "k_proj", "v_proj", "o_proj",
-        "gate_proj", "up_proj", "down_proj",
-    ], # Remove QKVO if out of memory
-    lora_alpha = lora_rank,
-    use_gradient_checkpointing = "unsloth", # Enable long context finetuning
-    random_state = 3407,
-)
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r = lora_rank, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        target_modules = [
+            "q_proj", "k_proj", "v_proj", "o_proj",
+            "gate_proj", "up_proj", "down_proj",
+        ], # Remove QKVO if out of memory
+        lora_alpha = lora_rank,
+        use_gradient_checkpointing = "unsloth", # Enable long context finetuning
+        random_state = 3407,
+    )
+    return model, tokenizer
