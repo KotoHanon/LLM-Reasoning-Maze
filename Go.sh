9f1c25f398260e222ac49c1cc57b95cc84377c73
KotoHanon <148569017+KotoHanon@users.noreply.github.com>
2025-03-08 19:13:59 +0800

    Update Go.sh

--- Go.sh
+++ Go.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # 依次执行 python 文件
-python file1.py
-python file2.py
-python file3.py
+python Env/MazeTrainer.py # 生成数据集
+python DataProcess/Process.py # 对数据集进行清洗
+python Code/Trainer.py # 利用GRPO进行PEFT
+python Inference.py # LoRA合并，生成结果action_seq
+python Eval.py # 在Maze环境中进行评估（带有GUI）
